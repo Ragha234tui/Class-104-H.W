887b13bb212e2b016f61d1d4c45d67d3815e80c0
RAGHAV <104725044+Ragha234tui@users.noreply.github.com>
2023-06-08 13:32:39 +0530

    Add files via upload

---
+++ Class 104 h.w/PRO-104-Project-Image-main/Solar_planet.py
@@ -0,0 +1,78 @@
+import cv2
+
+img=cv2.imread('D:/USER/Desktop/Python Programm/Class 104 h.w/PRO-104-Project-Image-main/solar-system.jpg')
+
+cv2.putText(img,
+            "Sun",
+            (20,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Mercury",
+            (90,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Venus",
+            (200,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Earth",
+            (300,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Mars",
+            (390,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Jupiter",
+            (500,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Saturn",
+            (750,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Uranus",
+            (970,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+            "Neptune",
+            (1111,170),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.imshow("output",img)
+cv2.waitKey(0)
